80305a1069c95f67b57b3e57b3a061a59c5100a4
Aryan095 <aryanshukla095@gmail.com>
2025-01-15 11:51:29 +0530

    add .env file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,11 @@
  
  import {Routes, Route} from "react-router-dom";
 
+ import {HomePage} from "./pages/HomePage";
+ import {SignUpPage} from "./pages/SignUpPage";
+ import {LoginPage} from "./pages/LoginPage";
+ import { ProfilePage } from './pages/ProfilePage';
+
  const App = () => {
    return (
      <div className="text-red-500"> 
@@ -12,10 +17,10 @@
      <Routes>
 
        <Route path="/" element={<HomePage />} />
-       <Route path="/signup" element={<HomePage />} />
-       <Route path="/login" element={<HomePage />} />
-       <Route path="/" element={<HomePage />} />
-       <Route path="/" element={<HomePage />} />
+       <Route path="/signup" element={<SignUpPage />} />
+       <Route path="/login" element={<LoginPage />} />
+       <Route path="/settings" element={<SettingsPage />} />
+       <Route path="/" element={<ProfilePage />} />
 
      </Routes>
 
